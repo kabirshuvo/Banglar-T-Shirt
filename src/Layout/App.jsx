629adc32adca7f018f182d6f22be90e649231aa9
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-07 02:06:40 +0600

    Footer Added

--- src/Layout/App.jsx
+++ src/Layout/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import './App.css'
 
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
             
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            All Right Reserved @2023 Banglar-t-shirt.com
+        </div>
+    );
+};
+
+export default Footer;
