28dfbc45e30c6e88077a1f23ae12062f604dced9
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-07 01:51:37 +0600

    Cart Section Added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
 
 
 const Home = () => {
     const tShirts = useLoaderData();
     return (
-        <div>
+        <div className='home-container'>
+        <div className='t-shirt-container'>
         {
             tShirts.map(tShirt => <TShirt key={tShirt._id} tShirt={tShirt}></TShirt>)
         }
+        </div>
+        <div className='cart-container'>
+            <Cart></Cart>
+        </div>
         </div>
     );
 };
--- src/components/TShirt/TShirt.jsx
+++ src/components/TShirt/TShirt.jsx
@@ -4,7 +4,12 @@ const TShirt = ({tShirt}) => {
     const{picture, name, price, gender} = tShirt;
     return (
         <div>
+            <h1>{name}</h1>
             <img src={picture} alt="" />
+            <div>
+                <p>Price: ${price}</p>
+                <p>{gender}</p>
+            </div>
         </div>
     );
 };
