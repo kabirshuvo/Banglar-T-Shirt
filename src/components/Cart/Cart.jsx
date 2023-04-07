3797982dc36faab82dec52398589f99bb018838d
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-07 17:32:52 +0600

    Conditional CSS applied

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Cart.css';
 const Cart = ({cart, handleRemoveFromCart }) => {
     let message;
     if(cart.length === 0){
@@ -10,7 +10,7 @@ const Cart = ({cart, handleRemoveFromCart }) => {
     return (
         <div>
             {message}
-            <h2>Order Summary {cart.length}</h2>
+            <h2 className={cart.length === 0 ? 'text-orange' : 'text-purple'}>Order Summary {cart.length}</h2>
             {cart.length > 2 ? <span>Double Another</span> : <span>Left the confution, Collect Some Products</span>}
             
             {
@@ -21,7 +21,7 @@ const Cart = ({cart, handleRemoveFromCart }) => {
                 cart.length === 3 && <p>You are elegible for bonus</p>
 
             }{
-                cart.length === 4 || <p>Great Choice, Keep Collection</p>
+                cart.length !== 4 || <p>Great Choice, Keep Collection</p>
             }
         </div>
     );
